ef9cd31dd6e4d1f0e2959a1ad2cf4f4d852c5d4f
frozegnome <pcaron@att.net>
2015-08-02 16:34:59 -0400

    Transfer work to continue on other machine.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,19 @@
-(function () {
+(function() {
 
 	'use strict';
 
-}());
+	$(document).ready(function() {
+
+		//animation for slide-pages
+		$('#circle-slide').on('click', function() {
+			$('body').scrollTo('#slide-page');
+		});
+
+		$('#flipbook').turn({
+			width: 400,
+			height: 300,
+			autoCenter: true
+		});
+	});
+
+})();
